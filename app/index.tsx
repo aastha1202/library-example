0565c38fe8ae26b5b2a388bc0a68b2015eab1ced
Aastha Singh <saastha2002@gmail.com>
2024-07-22 22:55:30 +0530

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,23 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
+
+export function newLibrary(){
+  const newfunc= () : string=>{
+    return 'new function'
+  }
+
+  return {newfunc}
+}
